44d63180bdd29ac47954d9a35b4d3b1eb57262e5
root <root@banff-sc-cx43-14.dh170.dcgpu>
2023-12-20 02:00:35 +0000

    add help_read_rocblas_bench.py and read_rocprof.csv

--- help_read_rocblas_bench.py
+++ help_read_rocblas_bench.py
@@ -1,20 +1,29 @@
-path = 'debug.log'
+#!/usr/bin/env python
+path = 'bloom7b_rocblas_6.log'
 
 file1 = open(path, 'r')
 Lines = file1.readlines()
-visited = set()
+visited = dict()
 for line in Lines:
-    if './rocblas-bench' in line.strip():
+    if './rocblas-bench' in line.strip() and 'Profiling' not in line.strip():
         tmp = line.strip()
         if tmp not in visited:
-            visited.add(tmp)
+            visited[tmp] = 1
+        else:
+            visited[tmp] += 1
 
 print(len(visited))
+
 res = list()
-for vi in visited:
-    tmp = vi.split()
-    key = (tmp[4], tmp[6], int(tmp[8]), int(tmp[10]), int(tmp[12]))
-    res.append(key)
-res.sort()
+for key in visited:
+    tmp = key.split()
+    print(tmp)
+    key_ = (tmp[4], tmp[6], int(tmp[8]), int(tmp[10]), int(tmp[12]))
+    tmp = (visited[key], key_)
+    res.append(tmp)
+res.sort(reverse=True)
+
 for r in res:
     print(r)
+
+print(len(visited))
